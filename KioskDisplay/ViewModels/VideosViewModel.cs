e926f373ba17cdc9fe507036ef98451f1bbb41bc
CaptainZod <jnewman30@gmail.com>
2015-03-31 18:12:29 -0500

    Video

---
+++ KioskDisplay/ViewModels/VideosViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace KioskDisplay.ViewModels
+{
+    public class VideosViewModel : ScrollerViewModel
+    {
+        protected override System.Windows.ResourceDictionary LoadContent()
+        {
+            return GetResourceDictionary("Resources/Videos.xaml");
+        }
+    }
+}
