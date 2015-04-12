3ebb796ae3fd293e415694c9cdb6158649b6897c
CaptainZod <jnewman30@gmail.com>
2015-04-12 18:19:26 -0500

    Add touch page and command interaction for video and image pages.

---
+++ KioskDisplay/Pages/TouchPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace KioskDisplay.Pages
+{
+    public abstract class TouchPage : Page
+    {
+        protected TouchPoint TouchStart;
+        protected bool AlreadySwiped = false;
+
+        public event EventHandler TouchSwipeLeft = delegate { };
+        public event EventHandler TouchSwipeRight = delegate { };
+
+        public TouchPage()
+        {
+            TouchDown += TouchPage_TouchDown;
+            TouchMove += TouchPage_TouchMove;
+        }
+
+        void TouchPage_TouchMove(object sender, TouchEventArgs e)
+        {
+            if(AlreadySwiped || TouchStart == null)
+            {
+                return;
+            }
+
+            var finalTouch = e.GetIntermediateTouchPoints(this).LastOrDefault();
+            if(finalTouch == null)
+            {
+                return;
+            }
+
+            // Swipe to right
+            if(finalTouch.Position.X > TouchStart.Position.X + 200)
+            {
+                AlreadySwiped = true;
+                try
+                {
+                    TouchSwipeLeft(this, new EventArgs());
+                }
+                finally
+                {
+                    AlreadySwiped = false;
+                }
+            }
+
+            // Swipe to left
+            if(finalTouch.Position.X < TouchStart.Position.X - 200)
+            {
+                AlreadySwiped = true;
+                try
+                {
+                    TouchSwipeRight(this, new EventArgs());
+                }
+                finally
+                {
+                    AlreadySwiped = false;
+                }
+            }
+        }
+
+        void TouchPage_TouchDown(object sender, TouchEventArgs e)
+        {
+            TouchStart = e.GetTouchPoint(this);
+        }
+    }
+}
--- KioskDisplay/Pages/Videos.xaml.cs
+++ KioskDisplay/Pages/Videos.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿
 namespace KioskDisplay.Pages
 {
     /// <summary>
     /// Interaction logic for Videos.xaml
     /// </summary>
-    public partial class Videos : Page
+    public partial class Videos : TouchPage
     {
         public Videos()
         {
